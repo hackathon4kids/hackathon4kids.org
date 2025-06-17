5516f993074788829a49aaa40a209ffa4c1d1bda
jowzow <116520246+jowzow@users.noreply.github.com>
2025-06-16 22:08:08 -0700

    Update JohnnyWang.tsx

--- src/app/about/JohnnyWang.tsx
+++ src/app/about/JohnnyWang.tsx
@@ -4,11 +4,11 @@ import johnnyWang from 'public/johnny_wang.jpg'
 const JohnnyWangCard = () => (
   <Card
     name="Johnny"
-    subtitle="Vice President, Instructor"
+    subtitle="President, Instructor"
     avatar={johnnyWang}
-    tags={[Tag.CurrentInstructor, Tag.Robotics]}
+    tags={[Tag.CurrentInstructor, Tag.APCSA, Tag.Robotics]}
   >
-    Johnny is a junior at Valley Christian High School that likes programming, robotics, and football. He has 2 years of  programming experience and 3 years of robotics experience. Johnny is currently the programming mentor for 2 middle school robotics teams and seeks to pass on his programming knowledge.
+    Johnny is a senior at Valley Christian High School that likes programming, robotics, and basketball. He has 4 years of  programming experience and 5 years of robotics experience. Johnny is currently the programming mentor for 2 middle school robotics teams and seeks to pass on his programming knowledge.
   </Card>
 )
 
